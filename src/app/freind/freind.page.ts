a5daa6b0f34f9106957a5698bd047128eacf94b8
jatur rahmah <kotijahturahmah.mhs.politala.ac.id>
2023-10-15 17:39:12 +0800

    fied:   src/app/freind/freind.page.ts
    git push

--- src/app/freind/freind.page.ts
+++ src/app/freind/freind.page.ts
@@ -12,14 +12,5 @@ export class FriendPage implements OnInit {
   ngOnInit() {
   }
 
-  friendList = [
-    { nama: 'Parkhyungseok', status: 'Add friend', img: "assets/img/park.jpeg"},
-    { nama: 'Vasco_id', status: 'Add friend', img: "assets/img/vasco.jpeg"},
-    { nama: 'Han_Seojun', status: 'Accept', img: "assets/img/seojun.jpeg"},
-    { nama: 'Lee_Suho', status: 'Accept', img: "assets/img/seo.jpeg"},
-    { nama: 'toro_M', status: 'Accept', img: "assets/img/toro.jpeg"},
-    { nama: 'janghyun_anak_satu', status: 'unfriend', img: "assets/img/jang.jpeg"},
-    { nama: 'sho_sayangibu', status: 'Add friend', img: "assets/img/sho.jpeg"},
-    { nama: 'dg_papanatas', status: 'Accept', img: "assets/img/dg.jpeg"},
-  ];
+ 
 }
